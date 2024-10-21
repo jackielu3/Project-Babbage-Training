e0857c2019c1a5932af26df7ec448ca2443b1126
Jackie Lu <jackielu3@gmail.com>
2024-10-21 09:44:08 -0700

    lab 4 complete!

--- lab4/helloWorldToken.js
+++ lab4/helloWorldToken.js
@@ -1,18 +1,20 @@
-const { createAction, toBEEFfromEnvelope } = require("@babbage/sdk-ts");
-const { HelloTokens } = require("hello-tokens");
+const { createAction, toBEEFfromEnvelope } = require('@babbage/sdk-ts');
+const { HelloTokens } = require('hello-tokens');
 
 (async () => {
   try {
-    const script = await HelloTokens.createOutputScript("Hello World");
-
+    const script = await HelloTokens.createOutputScript('Hello Bockchain!');
+    const overlayURL = 'https://staging-overlay.babbage.systems'
+    
     const createActionResult = await createAction({
       outputs: [
         {
           satoshis: 1,
           script,
+          description: 'New HelloWorld token'
         },
       ],
-      description: "Create a new Hello World token",
+      description: 'Create a new Hello World token',
     });
 
     const beef = toBEEFfromEnvelope({
@@ -22,27 +24,27 @@ const { HelloTokens } = require("hello-tokens");
     }).beef;
 
     const submitResult = await fetch(
-      "https://staging-overlay.babbage.systems/submit",
+      `${overlayURL}/submit`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/octet-stream",
-          "X-Topics": JSON.stringify(["tm_helloworld"]),
+          'Content-Type': 'application/octet-stream',
+          'X-Topics': JSON.stringify(['tm_helloworld']),
         },
         body: new Uint8Array(beef),
       }
     );
 
     const lookupResult = await fetch(
-      "https://staging-overlay.babbage.systems/lookup",
+      `${overlayURL}/lookup`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          service: "ls_helloworld",
-          query: "Hello World",
+          service: 'ls_helloworld',
+          query: 'Hello Blockchain!',
         }),
       }
     );
@@ -53,17 +55,12 @@ const { HelloTokens } = require("hello-tokens");
 
     const lookupAnswer = await lookupResult.json()
 
-    console.log("Lookup Answer (Raw):", lookupAnswer);
+    // console.log('Lookup Answer:', lookupAnswer)
 
-    if (!lookupAnswer || !lookupAnswer.message) {
-      throw new Error("Unexpected lookup answer structure.");
-    }
-
-    
     const message = await HelloTokens.parseLookupAnswer(lookupAnswer)
 
-    console.log("Parsed Message:", message);
+    console.log('Parsed Message:', message);
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 })();
